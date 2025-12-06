d16825c11ec0fc7729ad47f889bb73a821587c42
zfc <zhuliderb@gmail.com>
2025-12-06 20:58:09 +0800

    feat: add web analytics

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import DailyPage from './pages/DailyPage';
 import HomePage from './pages/HomePage';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
         <Route path="/lesson/:lessonId" element={<DailyPage />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
+      <Analytics />
     </Router>
   );
 }
